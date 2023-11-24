e7ace57dd9b86fe1e7f973bff2363c82ab04ba5f
nihad-hh <hodzicnihad10@gmail.com>
2023-11-24 11:32:57 +0100

    Added go back, go forward and redirect.

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,29 @@
     <router-link :to="{ name: 'about' }">About</router-link>
     <router-link :to="{ name: 'jobs' }">Jobs</router-link>
   </nav>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
   <router-view />
 </template>
 
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: "home" });
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    forward() {
+      this.$router.go(1);
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Helvetica, Arial, sans-serif;
